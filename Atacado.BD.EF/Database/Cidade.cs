8878b5960821bcf0edb0ffbc0e51f9e65f49210f
mayklopes <74882737+mayklopes@users.noreply.github.com>
2023-03-29 21:43:55 -0400

    Add files via upload

---
+++ Atacado.BD.EF/Database/Cidade.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace Atacado.BD.EF.Database;
+
+[Table("Cidade")]
+
+public partial class Cidade
+{
+    [Key]
+
+    public Int64 CodigoCidade { get; set;}
+
+
+    [Unicode(false)]
+    public string Nome { get; set; } = null!;
+
+    public Int64 CodigoIBGE7 { get; set;}
+
+    [Unicode(false)]
+    public string UF { get; set; } = null!;
+
+    public Int64 CodigoEstado { get; set;}
+
+    public Cidade()
+    {}
+}
---
+++ Atacado.BD.EF/Database/Estado.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace Atacado.BD.EF.Database;
+
+[Table("Estado")]
+
+public partial class Estado
+{
+   
+    [Key]
+
+    public Int64 CodigoEstado { get; set;}
+
+    [Unicode(false)]
+    public string Nome { get; set; } = null!;
+
+    [Unicode(false)]
+    public string UF { get; set; } = null!;
+
+    public Int64 CodigoRegiao { get; set;}
+
+    public Estado()
+    {}
+}
